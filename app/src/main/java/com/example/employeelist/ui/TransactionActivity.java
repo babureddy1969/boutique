a58b39b9c46c000316b5cd58828b191482e8eb96
Babu Reddy <babu.reddy@aspiredigital.com>
2020-05-23 11:11:17 +0530

    Modified sale screen.

--- app/src/main/java/com/example/employeelist/ui/TransactionActivity.java
+++ app/src/main/java/com/example/employeelist/ui/TransactionActivity.java
@@ -1,8 +1,11 @@
 package com.example.employeelist.ui;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+
+import androidx.appcompat.widget.AppCompatTextView;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
 import com.example.employeelist.R;
@@ -10,6 +13,7 @@ import com.example.employeelist.database.DatabaseHelper;
 import com.example.employeelist.model.TransactionModel;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.textfield.TextInputEditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -21,6 +25,7 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 import android.widget.EditText;
 
 import java.text.SimpleDateFormat;
@@ -33,7 +38,7 @@ public class TransactionActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_transaction);
+        setContentView(R.layout.activity_sale);
         dbHelper =  new DatabaseHelper(getApplicationContext());
 //        Toolbar toolbar = findViewById(R.id.toolbar);
 //        setSupportActionBar(toolbar);
@@ -45,7 +50,7 @@ public class TransactionActivity extends AppCompatActivity {
             TransactionModel t = dbHelper.getTransaction(value);
             setData(t);
         }
-        FloatingActionButton fab = findViewById(R.id.fab);
+        Button fab = findViewById(R.id.editSave);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
---
+++ app/src/main/java/com/example/employeelist/ui/activity_sale.java
@@ -0,0 +1,16 @@
+package com.example.employeelist.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.employeelist.R;
+
+public class activity_sale extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sale);
+    }
+}
